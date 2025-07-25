4b2f4d41c7ff54821c6265a7b4d0fbff2cda2871
Dan Foster-Smith <daniel.fostersmith@gmail.com>
2025-07-25 01:26:42 +0100

    Update manifest API URL to use HTTPS

--- src/app/manifest.service.ts
+++ src/app/manifest.service.ts
@@ -8,7 +8,7 @@ export class ManifestService {
 
   constructor(private http: HttpClient) { }
    data: any;
-   configUrl: string = "http://api-ecppec.ncl.ac.uk/api/manifests/e250cf21-a2d6-4a9d-ae7b-ae0c4420bda1";
+   configUrl: string = "https://api-ecppec.ncl.ac.uk/api/manifests/e250cf21-a2d6-4a9d-ae7b-ae0c4420bda1";
   getData(){
     this.data= "Test";
     return  this.http.get<any>(this.configUrl);
