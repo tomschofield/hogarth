d2b1d6b9d2bcb071e60d61d40a564f3905c3a155
Dan Foster-Smith <daniel.fostersmith@gmail.com>
2026-05-26 17:06:27 +0100

    Fix: view reload 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -59,7 +60,8 @@ import { ChatService } from './chat.service';
     MatSelectModule
   ],
   providers: [
-    ChatService
+    ChatService,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
